9146a6b405bcd2fb21722701bc9932d08c37502d
156572553@qq.com <156572553@qq.com>
2018-06-29 22:59:30 +0800

    ok

--- Algorithm算法/hanoi.py
+++ Algorithm算法/hanoi.py
@@ -5,13 +5,14 @@ def hanoi(n, x, y, z):
     global count
     if n == 1:  # 递归退出条件
         count += 1
-        print('移动步骤:', x, '--->', z,'已进行',count,'次')
+        print('已进行--',count,'次 此时--n= ' ,n)
+        print('移动步骤:--', x, '--->', z)
     else:
-
         count += 1
+        print('已进行',count,'次 此时n= ' ,n)
         hanoi(n-1, x, z, y)  # 将x上n-1个盘子借助z移动到y
-        # print('移动步骤:', x, '--->', z)
-        hanoi(1, x, y, z)  # 将x上最底下的盘子移动到z上
+        print('移动步骤:', x, '--->', z)
+        # hanoi(1, x, y, z)  # 将x上最底下的盘子移动到z上
         hanoi(n-1, y, x, z)  # 将y上n-1个盘子借助x移动到z
 
     return count
